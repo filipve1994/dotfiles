0afdb23ffae98e6cab0229efb695a04f74c87221
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2020-03-24 09:45:48 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -11,30 +15,27 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# brew cask install nvm
-# mkdir ~/.nvm
+# extra utils
+brew install tree
 
+# install terminal
+brew cask install iterm2
+
+#install browsers
 brew cask install firefox
 # brew cask install firefox-developer-edition
 brew cask install google-chrome
 # brew cask install google-chrome-canary
+
+# install tools
 brew cask install lastpass
 brew cask install vlc
 brew cask install spotify
 brew cask install whatsapp
+brew cask install adobe-creative-cloud
 
-#brew cask install iterm2
 brew cask install spectacle
-brew cask install microsoft-teams
-
-# sdk
-
-#without cask
-brew install maven
-brew tap AdoptOpenJDK/openjdk
-brew cask install adoptopenjdk14
-brew cask install adoptopenjdk14-jre
-# brew cask install yarn
+#brew cask install microsoft-teams
 
 
 #programming
@@ -54,6 +55,21 @@ brew cask install texstudio
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 
+# sdk
+
+#without cask
+brew install maven
+
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk14
+brew cask install adoptopenjdk14-jre
+# brew cask install yarn
+
 # Remove outdated versions from the cellar.
 
 brew cleanup
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+
