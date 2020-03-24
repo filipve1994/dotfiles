5ca9b459123f99400392d036c7f65f06aa2ad2c3
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2020-03-24 09:46:07 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -70,6 +70,7 @@ brew cask install adoptopenjdk14-jre
 brew cleanup
 
 # install nvm
+# https://github.com/nvm-sh/nvm#installing-and-updating
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 
